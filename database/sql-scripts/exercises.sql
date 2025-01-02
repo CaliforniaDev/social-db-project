e404943593c46d54c9933bf739d0b39bd0a07373
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-01-02 02:18:12 -0800

    fix: Correct typos and improve SQL query formatting in exercises.sql

--- database/sql-scripts/exercises.sql
+++ database/sql-scripts/exercises.sql
@@ -1,6 +1,4 @@
-
-
--- Show the users who have the Top 3 highst ID numbers.
+-- Show the users who have the Top 3 highest ID numbers.
 SELECT *
 FROM users
 ORDER BY id DESC
@@ -8,15 +6,14 @@ LIMIT 3;
 
 -- Show the username of user ID 200 abd the caption of all the posts that user has made.
 SELECT users.username, posts.caption
-FROM users
-JOIN posts
-ON users.id = posts.user_id
+FROM
+  users
+  JOIN posts ON users.id = posts.user_id
 WHERE users.id = 200;
 
 -- Show number of liker per user.
 SELECT users.username, COUNT(*)
-FROM users
-JOIN likes
-ON users.id = likes.user_id
+FROM
+  users
+  JOIN likes ON users.id = likes.user_id
 GROUP BY users.id;
-
