a928a812d7cb3e198cdea3ff58d7453ba81afeb6
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-01-05 20:08:45 -0800

    add: Create explain-analyze.sql for benchmarking query execution plans

---
+++ database/sql-scripts/benchmarking/explain-analyze.sql
@@ -0,0 +1,30 @@
+/*
+** Explain: Build a query that will return the execution plan of a query.
+** Explain Analyze: Build a query, run it, and return the execution plan of a query.
+** 
+** These are for benchmarking purposes.
+*/
+
+/*
+Query Plan (EXPLAIN ANALYZE Output): 
+Hash Join  (cost=8.31..1795.11 rows=11 width=81) (actual time=1.003..19.286 rows=7 loops=1)
+  Hash Cond: (comments.user_id = users.id)
+  ->  Seq Scan on comments  (cost=0.00..1628.10 rows=60410 width=72) (actual time=0.031..8.530 rows=60410 loops=1)
+  ->  Hash  (cost=8.30..8.30 rows=1 width=17) (actual time=0.394..0.396 rows=1 loops=1)
+        Buckets: 1024  Batches: 1  Memory Usage: 9kB
+        ->  Index Scan using users_username_idx on users  (cost=0.28..8.30 rows=1 width=17) (actual time=0.104..0.107 rows=1 loops=1)
+              Index Cond: ((username)::text = 'Alyson14'::text)
+Planning Time: 2.091 ms
+Execution Time: 19.436 ms
+*/ ;
+
+EXPLAIN ANALYZE
+  SELECT users.username, comments.contents
+  FROM
+    users
+    JOIN comments ON comments.user_id = users.id
+  WHERE users.username = 'Alyson14';
+
+SELECT *
+FROM pg_stats
+WHERE tablename = 'users';
