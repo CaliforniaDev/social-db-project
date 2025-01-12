1bbbcb7b6ce756be01a8dcd3cd8b7fc6ac9264c3
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-01-12 05:04:39 -0800

    add: Create materialized view for weekly likes aggregation and include refresh logic

--- database/sql-scripts/materialized-views/example.sql
+++ database/sql-scripts/materialized-views/example.sql
@@ -5,6 +5,7 @@
   - Gets executed only at specific times
   - Results are stored in the database
   - We can retrieve the results from the database without executing the query again
+  - If data changes, we need to refresh the materialized view
 
   - We use view anytime we have an expensive query that we want to run multiple times
 
@@ -15,17 +16,29 @@
 * Exercise 1: 
 * For each week, show the number of likes that posts and comments received.
 * Use the post and comment create_at date, not when the like was received.
-week 
 */ ;
 
-SELECT
-  -- DATE_TRUNC truncates the date to the specified interval
-  DATE_TRUNC('week', COALESCE(posts.created_at, comments.created_at)) AS week, -- COALESCE returns the first non-null value
-  COUNT(posts.id) AS num_post, -- COUNT returns the number of non-null values
-  COUNT(comments.id) AS num_comments -- COUNT returns the number of non-null values
-FROM
-  likes
-  LEFT JOIN posts ON likes.post_id = posts.id
-  LEFT JOIN comments ON likes.comment_id = comments.id
-GROUP BY week
-ORDER BY week;
+CREATE MATERIALIZED VIEW weekly_likes AS
+  (
+    SELECT
+      -- DATE_TRUNC truncates the date to the specified interval
+      DATE_TRUNC(
+        'week',
+        COALESCE(posts.created_at, comments.created_at) -- COALESCE returns the first non-null value
+      ) AS week,
+      COUNT(posts.id) AS num_likes_for_posts, -- COUNT returns the number of non-null values
+      COUNT(comments.id) AS num_likes_for_comments -- COUNT returns the number of non-null values
+    FROM
+      likes
+      LEFT JOIN posts ON likes.post_id = posts.id
+      LEFT JOIN comments ON likes.comment_id = comments.id
+    GROUP BY week
+    ORDER BY week
+  ) WITH DATA; -- WITH DATA populates the materialized view with the current data
+
+SELECT * FROM weekly_likes;
+
+DELETE FROM posts
+WHERE created_at < '2010-02-01';
+
+REFRESH MATERIALIZED VIEW weekly_likes; -- REFRESH updates the materialized view with the current data
